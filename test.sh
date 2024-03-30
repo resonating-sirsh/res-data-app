fa9d13b44a500e934fb4138f51661cd4829255e6
resonating-sirsh <amartey@gmail.com>
2024-03-30 14:37:10 -0400

    stuff

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
-my_list=["item1", "item2", "item3"]
+list='[a,b,c]'
 
-for item in "${my_list[@]}"; do
+items=$(echo "$list" | tr -d '[]' | tr ',' '\n')
+
+# Read the items into an array
+readarray -t items_array <<< "$items"
+
+# Loop over the items
+for item in "${items_array[@]}"; do
     echo "Processing item: $item"
     # Add your processing logic here
 done
